f655ec805a38f91ae94321a5275a525d53a54fff
NicolasIannello <nicolas.iannello99@gmail.com>
2025-06-13 12:54:48 -0300

    initial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './componentes/landing/landing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LandingComponent, children: [
+        //{ path: '', component: InicioComponent },
+    ] },
+    // { path: 'panelAdmin', component: PanelAdminComponent , children: [
+    //     { path: '', component: LoginComponent },
+    //     { path: 'usuarios', component: UsuariosComponent, canActivate:[authGuard] },
+    //     { path: 'autos', component: AutosComponent, canActivate:[authGuard] },
+    //     { path: 'Term&Cond', component: TycComponent, canActivate:[authGuard] },
+    //     { path: '**',   redirectTo: '', pathMatch: 'full' },
+    // ]},
+    { path: '**',   redirectTo: '', pathMatch: 'full' },
+];
