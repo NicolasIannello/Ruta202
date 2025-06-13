d425dcf9a946e72b652c068f529466e30e4be79a
NicolasIannello <nicolas.iannello99@gmail.com>
2025-06-13 16:13:19 -0300

    initial

--- src/app/componentes/landing/landing.component.ts
+++ src/app/componentes/landing/landing.component.ts
@@ -1,13 +1,18 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, ViewportScroller } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
 export class LandingComponent {
+  servicios:Array<string>=["Aseguradoras","Brokers de Seguros","Productores de Seguros","Estudios Liquidadores de Siniestros",
+    "Concesionarios del Automotor","Agencias revendedoras de Autos","Talleres Mecánicos y de Carrocería",]
+
+  constructor(public scroller: ViewportScroller) {}
 
 }
