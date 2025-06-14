c13f30d6a26d1a16e17483cd8eb08105157dc00f
NicolasIannello <nicolas.iannello99@gmail.com>
2025-06-14 14:54:31 -0300

    nav behavior

--- src/app/componentes/navbar/navbar.component.ts
+++ src/app/componentes/navbar/navbar.component.ts
@@ -1,15 +1,25 @@
-import { ViewportScroller } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { CommonModule, ViewportScroller } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
-  constructor(public scroller: ViewportScroller) {}
+export class NavbarComponent implements OnInit{
+  pagina:string='/';
+  nav:string='';
 
+  constructor(public scroller: ViewportScroller, private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe((event: NavigationEnd) => {
+        this.pagina=event.urlAfterRedirects;
+        this.nav='';
+    });
+  }
 }
