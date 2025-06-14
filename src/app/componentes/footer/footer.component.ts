8e1b0b7faddf71ae044387ac9732cded3f4e2587
NicolasIannello <nicolas.iannello99@gmail.com>
2025-06-14 14:48:55 -0300

    scroll classes

--- src/app/componentes/footer/footer.component.ts
+++ src/app/componentes/footer/footer.component.ts
@@ -1,16 +1,15 @@
 import { ViewportScroller } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterModule],
+  imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrls: ['./footer.component.css','../navbar/navbar.component.css']
 })
 export class FooterComponent {
   mail:string='remolques@ruta202.com.ar';
-  
+
   constructor(public scroller: ViewportScroller) {}
 }
