3c9824c98ca7a6251e8822fc10abb28230ca75cd
NicolasIannello <nicolas.iannello99@gmail.com>
2025-06-15 17:03:06 -0300

    selector tipo cuenta

--- src/app/componentes/registro/registro.component.ts
+++ src/app/componentes/registro/registro.component.ts
@@ -1,13 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './registro.component.html',
   styleUrl: '../login/login.component.css'
 })
 export class RegistroComponent {
-
+  type:number=0
 }
