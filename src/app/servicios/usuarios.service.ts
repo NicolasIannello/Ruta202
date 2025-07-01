9863c9af0bbdd5b12bcfd5f3a5d84cb98d1ac259
NicolasIannello <nicolas.iannello99@gmail.com>
2025-07-01 18:48:05 -0300

    moved

--- src/app/servicios/usuarios.service.ts
+++ src/app/servicios/usuarios.service.ts
@@ -2,7 +2,7 @@ import { HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../../environments/environment';
+import { environment } from '../../environments/environment';
 
 const base_url=environment.base_url;
 
