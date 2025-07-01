24811ae8f4a8f71c4a418f9bd39ec45a964fbae7
NicolasIannello <nicolas.iannello99@gmail.com>
2025-07-01 16:09:56 -0300

    api crearusuario

---
+++ src/app/componentes/servicios/usuarios.service.ts
@@ -0,0 +1,33 @@
+import { HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+const base_url=environment.base_url;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuariosService {
+  header:HttpHeaders;
+
+  constructor(private http: HttpClient) {
+    this.header=new HttpHeaders().set('Acces-Control-Allow-Origin','*');
+  }
+
+  async crearUsuario(dato:any){
+    try {
+      const resp = await fetch(base_url+'/usuarios/crearUsuario',{
+        method: 'POST', 
+        headers: {'Acces-Control-Allow-Origin':'*'},
+        body: dato
+      });
+
+      const data = await resp.json();
+      return data;
+    } catch (error) {
+      return false;
+    }
+  }
+}
