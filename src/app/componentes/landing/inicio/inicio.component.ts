20799d6ff150bf0119a560a2aa4e780a16ba3cdc
NicolasIannello <nicolas.iannello99@gmail.com>
2025-07-03 12:54:11 -0300

    routerLink

--- src/app/componentes/landing/inicio/inicio.component.ts
+++ src/app/componentes/landing/inicio/inicio.component.ts
@@ -4,11 +4,12 @@ import { NosotrosComponent } from "../nosotros/nosotros.component";
 import { ContactoComponent } from "../contacto/contacto.component";
 import { CommonModule, ViewportScroller } from '@angular/common';
 import { CommonService } from '../../../servicios/common.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [CommonModule, ServiciosComponent, NosotrosComponent, ContactoComponent],
+  imports: [CommonModule, ServiciosComponent, NosotrosComponent, ContactoComponent, RouterModule],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
