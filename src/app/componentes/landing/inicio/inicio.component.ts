97eb0597fd39e96739aa0fc2f0a1539caa7368fc
NicolasIannello <nicolas.iannello99@gmail.com>
2025-07-05 17:19:16 -0300

    flag botones

--- src/app/componentes/landing/inicio/inicio.component.ts
+++ src/app/componentes/landing/inicio/inicio.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { ServiciosComponent } from "../servicios/servicios.component";
 import { NosotrosComponent } from "../nosotros/nosotros.component";
 import { ContactoComponent } from "../contacto/contacto.component";
 import { CommonModule, ViewportScroller } from '@angular/common';
 import { CommonService } from '../../../servicios/common.service';
 import { RouterModule } from '@angular/router';
+import { UsuariosService } from '../../../servicios/usuarios.service';
+import { filter, first } from 'rxjs';
 
 @Component({
   selector: 'app-inicio',
@@ -13,7 +15,14 @@ import { RouterModule } from '@angular/router';
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
-export class InicioComponent {
-  constructor(public scroller: ViewportScroller, public common: CommonService) {}
+export class InicioComponent implements AfterViewInit{
+  flag:number=0;
 
+  constructor(public scroller: ViewportScroller, public common: CommonService, private api: UsuariosService) {}
+
+  ngAfterViewInit(): void {
+    this.api.ready$.pipe(filter(isReady => isReady),first()).subscribe(() => {
+      this.flag=this.api.getEmpresa()=='' ? 1 : 2;
+    });
+  }
 }
