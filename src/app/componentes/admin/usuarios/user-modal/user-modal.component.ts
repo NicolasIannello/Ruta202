4d5d06cf4bf4fb34f9ca5a305a3c24b654bbe98e
NicolasIannello <nicolas.iannello99@gmail.com>
2025-07-08 22:26:30 -0300

    edit text fields

--- src/app/componentes/admin/usuarios/user-modal/user-modal.component.ts
+++ src/app/componentes/admin/usuarios/user-modal/user-modal.component.ts
@@ -2,23 +2,33 @@ import { isPlatformBrowser, KeyValuePipe } from '@angular/common';
 import { Component, EventEmitter, Inject, Input, OnInit, Output, PLATFORM_ID } from '@angular/core';
 import { AdminService } from '../../../../servicios/admin.service';
 import { CommonService } from '../../../../servicios/common.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-modal',
   standalone: true,
-  imports: [KeyValuePipe],
+  imports: [KeyValuePipe, FormsModule],
   templateUrl: './user-modal.component.html',
   styleUrl: './user-modal.component.css'
 })
 export class UserModalComponent implements OnInit{
   @Output() messageEvent = new EventEmitter<null>();
-  @Input() userModal:any;
-  dato:any;
+  @Input() userModal:Record<string, string> = {};
+  dato:Record<string, string> = {};
   img:{[key: string]: Array<string>}={
     vehiculo: [],
     frente: [],
     dorso: []
   };
+  @Input() edit:boolean=false;
+  user2:Record<string, string> = {};
+  dato2:Record<string, string> = {};
+  sources: Array<any> = [];
+  imgs: any = [];
+  frente: any = {};
+  imgFrente: any = [];
+  dorso: any = {};
+  imgDorso: any = [];
 
   constructor(@Inject(PLATFORM_ID) private platformId: Object, private api: AdminService, private api2:CommonService) {}
     
@@ -34,6 +44,8 @@ export class UserModalComponent implements OnInit{
           next: (value:any) => {
             if (value.ok) {      
               this.dato=value.datoDB
+              this.user2= Object.assign( { }, this.userModal);
+              this.dato2= Object.assign( { }, this.dato);
               for (let i = 0; i < value.imgs.length; i++) {
                 if(value.imgs[i].tipo=='vehiculo'){
                   this.api2.getImg(value.imgs[i].img, '', '').then(resp=>{
@@ -56,5 +68,11 @@ export class UserModalComponent implements OnInit{
         });
       }
     }
+  }
+
+  cancelar(){
+    this.edit=false;
+    this.user2= Object.assign( { }, this.userModal);
+    this.dato2= Object.assign( { }, this.dato);
   }
 }
