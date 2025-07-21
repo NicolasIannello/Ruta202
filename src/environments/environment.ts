2b29f2a322d31b5b99c7d916402496772e059169
NicolasIannello <nicolas.iannello99@gmail.com>
2025-07-21 13:48:00 -0300

    5060

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   //base_url:'https://ruta202.com.ar//ruta',
-  base_url:'http://localhost:5000/ruta',
+  base_url:'http://localhost:5060/ruta',
   numero: '+5491127605336',
   numeroString: '+54 9 11 2760-5336',
 };
