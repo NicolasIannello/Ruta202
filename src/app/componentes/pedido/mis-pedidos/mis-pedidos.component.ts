7a6bdeb479c66d3276e8a7b9ddec41cbc9965d1a
NicolasIannello <nicolas.iannello99@gmail.com>
2025-10-14 12:09:19 -0300

    rezise

--- src/app/componentes/pedido/mis-pedidos/mis-pedidos.component.ts
+++ src/app/componentes/pedido/mis-pedidos/mis-pedidos.component.ts
@@ -93,8 +93,8 @@ export class MisPedidosComponent implements OnInit{
     const canvas = this.canvasRef.nativeElement;
     // Resize for HiDPI
     const ratio = Math.max(window.devicePixelRatio || 1, 1)==2 ? 1 : Math.max(window.devicePixelRatio || 1, 1);    
-    canvas.width = window.innerWidth<500 ? (window.innerWidth-100) * ratio : 500 * ratio;
-    canvas.height = 200 * ratio;
+    canvas.width = window.innerWidth<500 ? (window.innerWidth*0.70) * ratio : 500 * ratio;
+    canvas.height = window.innerHeight<180 ? (window.innerWidth*0.60) * ratio : 200 * ratio;
     const ctx = canvas.getContext('2d')!;
     ctx.scale(ratio, ratio);
     this.pad = new SignaturePad(canvas, { throttle: 0 });
